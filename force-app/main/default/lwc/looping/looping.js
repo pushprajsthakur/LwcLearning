75a533e7911ef9f3a595e9e37be1f68288e4d3aa
Pushpraj Thakur <pushprajthakur@outlook.com>
2022-05-27 22:04:18 +0530

    first commit

---
+++ force-app/main/default/lwc/looping/looping.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class Looping extends LightningElement {
+    carList = ["ford", "bmw", "ferrari", "kia", "maruti", "tata","baleno","thar"];
+}
